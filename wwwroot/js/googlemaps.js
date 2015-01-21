009625aa00c3feff2dce470a929405e6e17ce9fb
jamasu <suleimaj@ifi.uio.no.no>
2015-01-21 20:09:19 +0100

    google maps changes

--- wwwroot/js/googlemaps.js
+++ wwwroot/js/googlemaps.js
@@ -1,10 +1,18 @@
-  function initialize() {
-        var mapCanvas = document.getElementById('map-canvas');
-        var mapOptions = {
-          center: new google.maps.LatLng(59.93039062291619, 10.885745286941528),
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.HYBRID
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+
+
+      function initialize() {
+  var myLatlng = new google.maps.LatLng(59.93039062291619, 10.885745286941528);
+  var mapOptions = {
+    zoom: 10,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+ google.maps.event.addDomListener(window, 'load', initialize);
