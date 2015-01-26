feb11a890a126aafcc3dcfca1f6aeebf2abef408
jamasu <suleimaj@ifi.uio.no.no>
2015-01-26 13:19:15 +0100

    Google maps in contact is now loading with angularJS

--- wwwroot/js/googlemaps.js
+++ wwwroot/js/googlemaps.js
@@ -1,6 +1,8 @@
+(function() {
+  mapController.$inject = ['$scope', '$routeParams'];
+  function mapController($scope, $routeParams) {
+    $scope.initialize = function() {
 
-
-      function initialize() {
   var myLatlng = new google.maps.LatLng(59.93039062291619, 10.885745286941528);
   var mapOptions = {
     zoom: 10,
@@ -14,5 +16,9 @@
       map: map,
       title: 'Hello World!'
   });
+ }
+$scope.locationName = $routeParams.locationName;
 }
- google.maps.event.addDomListener(window, 'load', initialize);
+  angular.module("scotchApp").controller("mapController", mapController);
+ //google.maps.event.addDomListener(window, 'load', initialize);
+}());
